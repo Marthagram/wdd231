5dabfdb6d16bfb4935a1cf82805b1b08ba4157d2
marthagram <mrsmartha.otas@gmail.com>
2026-01-07 20:11:49 -0800

    edit

---
+++ scripts/date.js
@@ -0,0 +1,8 @@
+const year = document.querySelector("#currentyear");
+const firstParagraph = document.querySelector
+
+const today = new Date();
+
+year.innerHTML = `&copy ${today.getFullYear()}  🌼 Martha Itohan Otasowie 🌼 Nigeria`;
+
+document.getElementById("lastModified").innerHTML = ` Last Modification: ${ document.lastModified}`;
