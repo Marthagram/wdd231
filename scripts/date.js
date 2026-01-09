43dfead22dbff294e8c7cb8578453393a657d85c
marthagram <mrsmartha.otas@gmail.com>
2026-01-08 22:07:26 -0800

    week 1 project building

--- scripts/date.js
+++ scripts/date.js
@@ -1,9 +1,9 @@
 const year = document.querySelector("#currentyear");
-const firstParagraph = document.querySelector("p")
+
 
 const today = new Date();
 
-year.innerHTML = ` ${today.getFullYear()}  `;
-firstParagraph.innerHTML = `🌼 Martha Itohan Otasowie 🌼 Nigeria`
+year.innerHTML = `&copy; ${today.getFullYear()} 🌼 Martha Itohan Otasowie 🌼 Nigeria `;
+
 
 document.getElementById("lastModified").innerHTML = ` Last Modification: ${ document.lastModified}`;
